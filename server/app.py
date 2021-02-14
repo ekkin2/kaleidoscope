d8a2dc23c42b6017225da352cdbe5f1a43e86044
Gautham Raghupathi <gauthamar11@gmail.com>
2021-02-13 19:07:49 -0800

    Flask REST API to load db locally and respond to query

---
+++ server/app.py
@@ -0,0 +1,82 @@
+from flask import Flask, request, jsonify
+import json
+from newsapi import NewsApiClient
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+categories = {"business", "entertainment", "general", "health", "science", "sports", "technology"}
+sorts = {"recent", "objective", "polarity"}
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+# Pass 3 params: category, sort, and score (only needed for some cases)
+@app.route('/api/articles/', methods=['GET'])
+def get_recent():
+    if 'category' in request.args:
+        category = str(request.args['category'])
+        if category not in categories:
+            return category
+    else:
+        category = "general"
+
+    if 'sort' in request.args:
+        sort = str(request.args['sort'])
+        if sort not in sorts:
+            return "Error: Please specify an accurate sort"
+    else:
+        sort = "recent"
+
+    with open('db.json') as f:
+        data = json.load(f)
+
+    category_data = data[category]
+
+    if sort == "recent":
+        return category_data
+
+    if sort == "objective":
+        return sorted(category_data["articles"], key=lambda article : article['objectivity'], reverse=True)
+
+    if sort == "polarity":
+        user_polarity = request.args['score']
+        target_polarity = 100 - user_polarity
+
+        return sorted(category_data["articles"], key=lambda article : abs(target_polarity - article['polarity']))
+
+    return "not success"
+
+
+
+
+@app.route('/api/load')
+def refresh_db():
+    newsapi = NewsApiClient(api_key='fc821330ca0645379a9a1477fe76fa6a')
+    data = {}
+
+    for category in categories:
+        data[category] = newsapi.get_top_headlines(language='en',
+                                                  country='us',
+                                                  page_size=50,
+                                                  category=category)
+
+        for article in data[category]["articles"]:
+            # TODO Objectivity
+            objectivity = 100
+            # TODO Bias
+            polarity = 0
+
+            article['objectivity'] = objectivity
+            article['polarity'] = polarity
+
+    with open('db.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    return "success"
+
+
+
+if __name__ == '__main__':
+    app.run()
