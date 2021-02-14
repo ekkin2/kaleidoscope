467606f9763ed446693307c52895a7fa06c763ce
ekkin2 <ekintiu@gmail.com>
2021-02-13 18:20:07 -0600

    Make facts button larger

--- kaleidoscope/lib/widgets/news_card.dart
+++ kaleidoscope/lib/widgets/news_card.dart
@@ -133,7 +133,10 @@ class _NewsCardState extends State<NewsCard> {
     return Container(
         margin: EdgeInsets.only(right: 8),
         child: ExpandableButton(
-          child: Icon(Icons.list),
+          child: Icon(
+              Icons.list,
+            size: 28,
+          ),
         ),
       );
   }
